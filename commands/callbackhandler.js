032e955c7780acc62afdf045bc48b953c80735ac
icodelifee <mithunsmenon@gmail.com>
2020-12-05 17:16:16 +0530

    Fetch total_clicks from API instead of using wrapper [v4 Changes]

--- commands/callbackhandler.js
+++ commands/callbackhandler.js
@@ -1,28 +1,38 @@
-const TeleBot = require('telebot');
-const config = require('../config');
-const Bitly = require('bitlyapi');
-
-const userToken = config.bitlyToken;
-
+const TeleBot = require("telebot");
+const config = require("../config");
+const axios = require("axios");
 const bot = new TeleBot(config.botToken);
-const bitly = new Bitly(userToken);
+
+const instance = axios.create({
+  baseURL: "https://api-ssl.bitly.com/v4/bitlinks/",
+  timeout: 1000,
+  headers: { Authorization: "Bearer " + config.bitlyToken },
+});
 
 const callback = async (msg) => {
-	const regex = /(https:\/\/bit.ly)\/(\w+)/gm;
-	const str = msg.message.text;
-	const callbackURL = regex.exec(str)[0];
-	const res = await bitly.clicks(callbackURL);
-	const clicks = res.data.clicks[0].user_clicks;
-	callbackMsg = str.replace(/Clicks([\w\s\.].*)/m, 'Clicks : ' + clicks);
-	const replyMarkup = bot.inlineKeyboard([[bot.inlineButton('🔄Refresh Clicks', { callback: 'null' })]]);
-	const msgParams = {
-		chatId: msg.message.chat.id,
-		messageId: msg.message.message_id,
-	};
-	bot.editMessageText(msgParams, callbackMsg, {
-		parse: 'html',
-		replyMarkup,
-	}).catch((err) => console.log(err));
+  const regex = /(bit.ly)\/(\w+)/gm;
+  const str = msg.message.text;
+  const callbackURL = regex.exec(str)[0];
+  const clicks = await getTotalClicks(callbackURL);
+  callbackMsg = str.replace(/Clicks([\w\s\.].*)/m, "Clicks : " + clicks);
+  const replyMarkup = bot.inlineKeyboard([
+    [bot.inlineButton("🔄Refresh Clicks", { callback: "null" })],
+  ]);
+  const msgParams = {
+    chatId: msg.message.chat.id,
+    messageId: msg.message.message_id,
+  };
+  bot
+    .editMessageText(msgParams, callbackMsg, {
+      parse: "html",
+      replyMarkup,
+    })
+    .catch((err) => console.log(err));
 };
+async function getTotalClicks(callbackURL) {
+  const url = `${callbackURL}/clicks/summary`;
+  const res = await instance.get(url);
+  return res.data.total_clicks;
+}
 
 module.exports = callback;
